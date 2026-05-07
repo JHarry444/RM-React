d3e95214cbab653f13f5e0c81f28db8066117eba
jordan h <jharrison1@qa.com>
2026-05-07 10:05:12 +0100

    init commit

---
+++ src/App.tsx
@@ -0,0 +1,34 @@
+import Input from "./components/Input";
+import MyList from "./components/MyList";
+
+function App() {
+
+  function add(a: number, b: number) {
+    return a + b;
+  }
+
+
+  return (
+    // <> is a React Fragment, it allows us to return multiple 
+    // elements without adding an extra node to the DOM
+    <>
+      {/* In JSX/TSX you can embed expressions using curly braces */}
+      <p>{`2 + 3 = ${add(2, 3)}`}</p>
+      <p>{`3 + 2 = ${add(3, 2)}`}</p>
+      <p>{`4 + 1 = ${add(4, 1)}`}</p>
+      <p>1 + 1</p>
+      <p>{1 + 1}</p>
+
+      <section>
+        <h2>Custom components</h2>
+        <input type="text" />
+        <Input />
+        <MyList />
+      </section>
+
+
+    </>
+  )
+}
+
+export default App
---
+++ src/components/MyList.tsx
@@ -0,0 +1,11 @@
+function MyList() {
+    return (
+        <ul>
+            <li>Eggs</li>
+            <li>Milk</li>
+            <li>Ham</li>
+        </ul>
+    );
+}
+
+export default MyList;
