267fbeec75d0830262f12ebd20791aee40db248b
jordan h <jharrison1@qa.com>
2026-05-11 11:46:28 +0100

    useeffect

--- src/components/lifting-state/Trainers.tsx
+++ src/components/lifting-state/Trainers.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import AddTrainer from "./AddTrainer";
 import ListTrainers from "./ListTrainers";
 import "./trainers.css";
@@ -13,6 +13,21 @@ export type TrainerType = {
 function Trainers() {
     const [trainers, setTrainers] = useState<TrainerType[]>([]);
 
+
+    // will only get called when the component is mounted, so it will only fetch the trainers once
+    // mounted -> initial render
+    useEffect(() => {
+        async function fetchTrainers() {
+            try {
+                const res = await fetch("http://localhost:8080/trainers");
+                const data = await res.json();
+                setTrainers(data);
+            } catch (error) {
+                console.error(error);
+            }
+        }
+        fetchTrainers();
+    }, []);
 
     return (
         <>
